0b564dfce5a46ec9fb5b03792cd0b9a1198019a4
y2eun <kye6735@naver.com>
2024-08-20 10:17:49 +0900

    보기 > 확대-축소 서브메뉴 구현

--- WinFormAdv32/Form1.cs
+++ WinFormAdv32/Form1.cs
@@ -12,6 +12,8 @@ namespace WinFormAdv32
 {
     public partial class Form1 : Form
     {
+        public int PROGRESS_BAR_STEP { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
@@ -80,6 +82,35 @@ namespace WinFormAdv32
             if(ofd.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = System.IO.File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void 확대하기축소하기기본값복원ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripProgressBar1.Value = 50;
+        }
+
+        private void 확대ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (toolStripProgressBar1.Value + PROGRESS_BAR_STEP >= 100)
+            {
+                toolStripProgressBar1.Value = 100;
+            }
+            else
+            {
+                toolStripProgressBar1.Value += PROGRESS_BAR_STEP;
+            }
+        }
+
+        private void 축소ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(toolStripProgressBar1.Value < PROGRESS_BAR_STEP)
+            {
+                toolStripProgressBar1.Value = 0;
+            }
+            else
+            {
+                toolStripProgressBar1.Value -= PROGRESS_BAR_STEP;
             }
         }
     }
